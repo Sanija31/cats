c9e602d12efa14260dbe037ea02d4ca64017c94c
Sanija31 <122867614+Sanija31@users.noreply.github.com>
2023-02-07 10:47:43 +0200

    uu9

---
+++ skripti.js
@@ -0,0 +1,9 @@
+//alert("čau!")
+console.log("kaut kas")
+var logs = document.querySelector(".catazinas");
+var zina = document.querySelector("#zina");
+var vards = document.querySelector("#vards");
+function sutitZinu(){
+    comsole.log("poga darbojas");
+    console.log("ziņu ir: "+zina.value);
+}
